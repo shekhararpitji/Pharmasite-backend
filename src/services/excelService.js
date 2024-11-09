fce727d6bcc1918c008888964c7a0e96c9dd016a
mohd umair siddiqui <mohdumairsiddiqui00@gmail>
2024-11-09 19:04:05 +0530

    resolved bugs for large file

--- src/services/excelService.js
+++ src/services/excelService.js
@@ -13,6 +13,7 @@ exports.parseAndInsertExcel = async (filePath, type) => {
     }
   } catch (error) {
     console.log(error.message)
+    throw error
   }
 };
 
@@ -31,6 +32,7 @@ const preprocessFieldNames = (data) => {
 
 const processExcelFile = async (filePath, model) => {
   const batchSize = 1000;
+  const batches = [];
   let batch = [];
   
   try {
@@ -41,7 +43,6 @@ const processExcelFile = async (filePath, model) => {
     const headers = worksheet.getRow(1).values.slice(1);
   
     
-    const transaction = await sequelize.transaction();
     worksheet.eachRow({ includeEmpty: false }, async (row, rowNumber) => {
 
       if (rowNumber === 1) return; 
@@ -56,7 +57,7 @@ const processExcelFile = async (filePath, model) => {
           return
         }
         
-         if(headers[colNumber-1] ==='yearMonth'){
+        if(headers[colNumber-1] ==='yearMonth'){
           const value = cell.value.split(`'-`)[0]
           rowData[headers[colNumber - 1]] = cell.value;
           rowData["year"] = value;
@@ -69,23 +70,25 @@ const processExcelFile = async (filePath, model) => {
       });
       
       batch.push(rowData);
+      
       if (batch.length === batchSize) {
-        await processBatch(batch, transaction, model);
+        batches.push(batch)
         batch = []; 
         return;
       }
+      
     });
-
-  
+    
     if (batch.length > 0) {
-      await processBatch(batch, transaction, model);
+      batches.push(batch)
     }
-    await transaction.commit();
+    
+    await batchHandler(batches, model)
     console.log('Data inserted successfully!');
-
+    
   } catch (error) {
-    await transaction.rollback();
     console.error('Error processing the Excel file:', error);
+    throw error
   }
 };
 
@@ -97,4 +100,26 @@ async function processBatch(batchObject, transaction, model) {
     console.error('Error processing batch:', error);
     throw error;
   }
+}
+
+
+async function batchHandler(batches , model) {
+  
+  try {
+    
+    const transaction = await sequelize.transaction();
+
+    const batchPromises = batches.map( batch => {
+      return processBatch(batch, transaction, model)
+    })
+    
+    await Promise.all(batchPromises)
+    transaction.commit()
+
+  } catch (error) {
+    console.log(error)
+    throw error
+    transaction.rollback()
+  }
+
 }
